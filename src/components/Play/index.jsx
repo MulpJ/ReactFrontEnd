a753ca770c524259a922b22f3526d20a50a7899f
matiasJoao <joaovictor.a4@gmail.com>
2022-03-28 13:30:22 -0300

    seta na tela do play

--- src/components/Play/index.jsx
+++ src/components/Play/index.jsx
@@ -1,8 +1,10 @@
 import {axios} from "../../axios/api.jsx"
 import react, { useEffect } from "react"
-import { useParams, useNavigate} from "react-router-dom"
+import { useParams, useNavigate, Link} from "react-router-dom"
 import { useState } from "react"
 import './style.scss'
+import seta  from '../../assets/seta.png'
+
 
 // pagina de assistir os filmes
 export  function Play(){
@@ -13,26 +15,31 @@ export  function Play(){
         const token = localStorage.getItem("token");
       if (!token) return f("/login");
 
-    const inner = document.querySelector(".mainPlay")
+    const inner = document.querySelector(".iframe")
+
       // consumindo a api do google drive para buscar o filme pelo titulo, se o titulo corresponder 
       // aparece o filme se o titulo nao corresponder , aparece um video padrao.
       axios.get('https://danestourado.com/api/google-drive-api-open/api.php').then((res) =>{
         res.data.map((play) => {
             if (play.nome == verif.movie) {
                  const film = play.embed 
-                 inner.innerHTML = film
-
+                 inner.innerHTML = film 
               }    
           })
       })
-        
-
     })
     // estrutura HTML
     return(
+      
         <div className="mainPlay">
+          <div className="seta">
+          <Link to='/home'><img src={seta} /></Link>
+          </div>
+            <div className="iframe">
              <iframe width="600" height="400" src="https://www.youtube.com/embed/dQw4w9WgXcQ"   allow="autoplay;" allowfullscreen></iframe>
+             </div>
         </div>
+
     )
 }
 
